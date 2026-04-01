db698a134b04c817eea1e90c3b80ccff2a2e1090
vavr0 <david.vavrinek@gmail.com>
2026-04-01 08:18:07 +0200

    scheme resolve add mh-permuted

--- code/src/scheme.c
+++ code/src/scheme.c
@@ -6,5 +6,8 @@ const SchemeOps *scheme_resolve(const char *id) {
     if (!id || strcmp(id, "mh") == 0 || strcmp(id, "mh-classic") == 0) {
         return scheme_mh_get();
     }
+    if (strcmp(id, "mh-permuted") == 0) {
+        return scheme_mh_permuted_get();
+    }
     return NULL;
 }
